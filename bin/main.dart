dea07114af00e3569b2db4c7630afcca7c40c223
harrishuang <huangjinping1000@163.com>
2021-08-21 16:40:00 +0800

    基础训练

---
+++ bin/main.dart
@@ -0,0 +1,48 @@
+import 'dart:async';
+import 'dart:io';
+
+import 'package:Darthub/Darthub.dart' as Darthub;
+import 'package:Darthub/step1/Action.dart';
+import 'package:Darthub/step1/Animal.dart';
+import 'package:Darthub/step1/file/MyFile.dart';
+import 'package:Darthub/step1/http/MyHttp.dart';
+import 'package:Darthub/step1/mixins/Mixins1.dart';
+//import 'package:Darthub/step1/numbers.dart' as numbers;
+
+main(List<String> arguments) {
+//  print('Hello world: ${Darthub.calculate()}!');
+
+//  Animal animal = new Dog();
+//  animal.eat();
+
+//  Action action=new Action();
+//  print("main isolate start");
+//  action.getURL();
+//  print("main isolate stop");
+
+//  print('main Start');
+
+//  new Future.delayed(new Duration(seconds: 1), () {
+//    print('this is my task');
+//  });
+
+//  new Future(() {
+//    sleep(Duration(seconds: 5));
+//    print("5s task");
+//  });
+//  print('main end');
+
+//  Mixins minxins1 = new Mixins();
+//  minxins1.run();
+
+//  for (int i = 0; i < 1000; i++) {
+//    print("======1123==${i}=>>>>");
+//  }
+
+//  MyHttp http=new MyHttp();
+//  http.onStart();
+
+//  MyFile myFile=new MyFile();
+//  myFile.readFile();
+//  print("3123");
+}
